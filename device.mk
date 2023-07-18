af53bf1a32e5fb5ff15f1093b853e77e25406511
Mikhail Naganov <>
2023-07-18 15:50:00 +0300

    surya: Switch audio HAL to V7 Updated audio policy manager configuration files to comply with the changes in the XML schema.
    Bug: 142480271
    Test: atest VtsHalAudioV7_0TargetTest
    Test: atest VtsHalAudioEffectV7_0TargetTest
    Change-Id: I8e4b132e42b1f6603a3382fe970109ef701220b8

--- device.mk
+++ device.mk
@@ -32,8 +32,8 @@ USE_DEX2OAT_DEBUG := false
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@6.0-impl \
-    android.hardware.audio.effect@6.0-impl \
+    android.hardware.audio@7.0-impl \
+    android.hardware.audio.effect@7.0-impl \
     android.hardware.audio.service \
     android.hardware.bluetooth.audio@2.1-impl \
     android.hardware.soundtrigger@2.2-impl
